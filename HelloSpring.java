631c81494133e8d7f4a0fb0bc1071603d93df574
Kirill <kirill.alexeev18@gmail.com>
2023-10-19 10:18:08 -0400

    default merge in second

--- HelloSpring.java
+++ HelloSpring.java
@@ -1,8 +1,5 @@
 public class HelloSpring {
     public static void main(String[] args) {
-        System.out.println("Hello Spring");
-        System.out.println("Create new branch");
-        System.out.println("HelloWorld");
-        System.out.println("Merge in default");
+
     }
 }
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Mastercommit");
-        System.out.println("NewBranchDefaultComit");
-        System.out.println("New MasterCommit");
-        System.out.println("New Branch1.1 From Master");
-        System.out.println("New Commit From MAster");
-        System.out.println("Commit and push in git Remote Repo");
+
     }
 }
