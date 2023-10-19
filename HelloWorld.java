283d9efc470724ab2cee1a6438acf84ce9a0b76b
Kirill Alekseev <kirill.alexeev18@gmail.com>
2023-10-19 09:02:00 -0400

    New Branch1.1

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Mastercommit");
         System.out.println("New MasterCommit");
+        System.out.println("New Branch1.1 From Master");
     }
 }
