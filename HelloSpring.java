c524697fa6e54bb9a5e2cdd49829c212893b5fed
Kirill <kirill.alexeev18@gmail.com>
2023-10-19 11:18:49 -0400

    Revert "Broked  Commit"
    This reverts commit d8768da4421ab4d749f695f1485b2820ace230d3.

--- HelloSpring.java
+++ HelloSpring.java
@@ -2,7 +2,12 @@ public class HelloSpring {
     public static void main(String[] args) {
         System.out.println("Create Branch 1.");
         System.out.println("Create Branch 1.1");
-        System.out.println
+        System.out.println("Create Branch 1.2");
+        System.out.println("CreateBranch 2");
+        System.out.println("Create Branch 2.1");
+        System.out.println("Create Branch 2.2");
+        System.out.println("Default Commit after Creating All Branches");
+        System.out.println("New One Commit");
 
     }
 }
