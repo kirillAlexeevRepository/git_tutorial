d88219c4fd3c785c4432063b84c9b302886a0cac
Kirill Alekseev <kirill.alexeev18@gmail.com>
2023-10-19 08:58:35 -0400

    MasterCommit1

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("We Learn Git");
-        System.out.println("GitfourCommit");
-        System.out.println("We Learn Git");
-        System.out.println("GitfourCommit");
-        System.out.println("IdeaCommit");
-        System.out.println("DefaultCommit");
-        System.out.println("Master ParalelCommit");
-        System.out.println("New One MAster commit");
+        System.out.println("Mastercommit");
     }
 }
